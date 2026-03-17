04d819a2094d7b2603444150c45553679a8e42e8
papaicriativo-collab <papaicriativo@gmail.com>
2026-03-17 20:18:40 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+for (let índice = 0; índice <= 0 / 0; índice++) {
+    music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+}
+radio.sendNumber(0)
